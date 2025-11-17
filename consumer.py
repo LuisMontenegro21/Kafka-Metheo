9992e5fc016087692d9843acbb76a2ad1f128f54
LuisMontenegro21 <83621697+LuisMontenegro21@users.noreply.github.com>
2025-11-17 00:19:31 -0600

    Fix consumer

--- consumer.py
+++ consumer.py
@@ -1,7 +1,10 @@
 from kafka import KafkaConsumer
 import json
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
-def kafka_consumer(topic: str, bootstrap_server: str) -> tuple[list, list, list]:
+
+def kafka_consumer(topic: str, bootstrap_server: str) -> None:
 
     consumer = KafkaConsumer(
         topic,
@@ -12,12 +15,57 @@ def kafka_consumer(topic: str, bootstrap_server: str) -> tuple[list, list, list]
     )
     all_temp: list = []
     all_hum: list = []
-    all_wind: list = []
-    for message in consumer:
-        print("Received:", message)
-        all_temp.append(message['temperatura'])
-        all_hum.append(message['humedad'])
-        all_wind.append(message['direccion_viento'])
+    # all_wind: list = []
     
-    return all_temp, all_hum, all_wind
+    plt.style.use("seaborn-v0_8")
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8,6))
+
+
+    def update(frame):
+        records = consumer.poll(timeout_ms=200)
+
+
+        for tp, batch in records.items():
+            for msg in batch:
+                data = msg.value   
+
+                temp = data.get("temperatura")
+                hum = data.get("humedad")
+
+                
+                if temp is None or hum is None:
+                    continue
+
+                all_temp.append(temp)
+                all_hum.append(hum)
+
+
+        if not all_temp:
+            return
+        window_size = 30
+        temps_window = all_temp[-window_size:]
+        hums_window = all_hum[-window_size:]
+
+        ax1.clear()
+        ax1.plot(temps_window)
+        ax1.set_ylim(0, 120)
+        ax1.set_ylabel("Temperatura")
+        ax1.set_title("Temperatura (últimas lecturas)")
+
+        ax2.clear()
+        ax2.plot(hums_window)
+        ax2.set_ylim(0, 100)
+        ax2.set_ylabel("Humedad")
+        ax2.set_title("Humedad (últimas lecturas)")
+        
+    ax1.set_title("Temperatura")
+    ax1.set_ylim(0, 120)
+
+    ax2.set_title("Humedad")
+    ax2.set_ylim(0, 100)
+
+    ani = animation.FuncAnimation(fig, update, interval=1000)
+    plt.tight_layout()
+    plt.show()
+
 
--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from consumer import kafka_consumer
 import sys
 
 def main() -> None:
-    BOOTSTRAP_SERVER = "lab9.alumchat.lol:9092"
+    BOOTSTRAP_SERVER = "iot.redesuvg.cloud:9092"
     TOPIC = "21699"
     val = sys.argv[1]
     if val == "produce":
         kafka_producer(topic=TOPIC, bootstrap_server=BOOTSTRAP_SERVER)
     elif val == "consume":
-        temp, hum, wind = kafka_consumer(topic=TOPIC, bootstrap_server=BOOTSTRAP_SERVER)
+        kafka_consumer(topic=TOPIC, bootstrap_server=BOOTSTRAP_SERVER)
     else:
         print("Not a valid argument: use 'produce' and 'consume' only")
 
